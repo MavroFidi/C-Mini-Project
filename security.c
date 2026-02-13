395f0288fc82804f70cc90344f78aaf505e21332
MavroFidi <thepixypoxy@gmail.com>
2026-02-13 13:36:19 +0530

    Update security.c

--- security.c
+++ security.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 #include "security.h"
 
 void codeInitial() {
+  int a;
+  printf("How many people do you wish access for? ");
+  scanf("%d",&a);
 
+  
 }
