45fa2ef7abb4dca2b65231c07d6354e706894761
MavroFidi <thepixypoxy@gmail.com>
2026-02-15 14:33:17 +0530

    Replace access and reset codes with user codes

--- security.h
+++ security.h
@@ -1,7 +1,15 @@
 #ifndef SECURITY_H
 #define SECURITY_H
 
-extern unsigned char accessCode[256];
-extern unsigned char resetCode[256];
+extern unsigned char userCode1[50];
+extern unsigned char userCode2[50];
+extern unsigned char userCode3[50];
+extern unsigned char userCode4[50];
+extern unsigned char userCode5[50];
+extern unsigned char userCode6[50];
+extern unsigned char userCode7[50];
+extern unsigned char userCode8[50];
+extern unsigned char userCode9[50];
+extern unsigned char userCode10[50];
 
 #endif
