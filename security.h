1d02f721d6e0ec43a5cd875b311535807e0cd914
MavroFidi <thepixypoxy@gmail.com>
2026-02-15 18:04:44 +0530

    Refactor security.h to manage user codes dynamically
    Removed multiple userCode arrays and added functions for initialization and cleanup.

--- security.h
+++ security.h
@@ -1,15 +1,9 @@
 #ifndef SECURITY_H
 #define SECURITY_H
 
-extern unsigned char userCode1[50];
-extern unsigned char userCode2[50];
-extern unsigned char userCode3[50];
-extern unsigned char userCode4[50];
-extern unsigned char userCode5[50];
-extern unsigned char userCode6[50];
-extern unsigned char userCode7[50];
-extern unsigned char userCode8[50];
-extern unsigned char userCode9[50];
-extern unsigned char userCode10[50];
+#define CODE_LENGTH 50
+
+unsigned char (*initializeCodes(int numArrays))[CODE_LENGTH];
+void cleanupCodes(void *ptr);
 
 #endif
