068f24d19393d4f394f89942b2aa7aa1dddacd17
MavroFidi <thepixypoxy@gmail.com>
2026-02-17 21:30:17 +0530

    Reduce adminCode and resetCode array sizes

--- admin.h
+++ admin.h
@@ -1,8 +1,8 @@
 #ifndef ADMIN_H
 #define ADMIN_H
 
-extern unsigned char adminCode[223];
-extern unsigned char resetCode[223];
+extern unsigned char adminCode[222];
+extern unsigned char resetCode[222];
 
 
 #endif
