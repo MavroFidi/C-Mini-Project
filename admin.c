5c0fb1a630321e715dbcaf9ed1054c52beaaee60
MavroFidi <thepixypoxy@gmail.com>
2026-02-17 21:30:03 +0530

    Refactor adminCode and resetCode handling
    Updated the size of adminCode and resetCode arrays, modified shuffle and printcode functions to accommodate the new sizes, and adjusted the main function to initialize and shuffle resetCode.

--- admin.c
+++ admin.c
@@ -3,12 +3,10 @@
 #include <time.h>
 #include "admin.h"
 
-unsigned char adminCode[256];
-unsigned char resetCode[16];
 
-void shuffle(unsigned char adminCode[256]) {
+void shuffle(unsigned char adminCode[222]) {
 
-    for (int i = 255; i > 0; i--) {
+    for (int i = 221; i > 0; i--) {
         int j = rand() % (i + 1);
 
         unsigned char temp = adminCode[i];
@@ -17,23 +15,8 @@ void shuffle(unsigned char adminCode[256]) {
     }
 }
 
-void resetCodeInitial(unsigned char adminCode[256]) {
-    for (int i = 0; i < 256; i+=16) {  
-            resetCode[i/16] = adminCode[i];
-    }
-
-    for (int i = 15; i > 0; i--) {
-        int j = rand() % (i + 1);
-
-        unsigned char temp = resetCode[i];
-        resetCode[i] = resetCode[j];
-        resetCode[j] = temp;
-    }
-
-}
-
-void printcode(adminCode[256]) {
-    for (int i = 0; i < 256; i++) {
+void printcode(adminCode[222]) {
+    for (int i = 0; i < 222; i++) {
         
         printf("%c\n",(char)adminCode[i]);
     }
@@ -42,11 +25,19 @@ void printcode(adminCode[256]) {
 void main() {
     srand(time(NULL));
 
-    for (int i = 0; i < 256; i++) {
-        adminCode[i] = (unsigned char)i;
+    int j = 0;
+    for (int i = 33; i < 256; i++) {
+            adminCode[j] = (unsigned char) i;
+            j++;
+    }
+    int j = 0;
+    for (int i = 33; i < 256; i++) {
+            resetCode[j] = (unsigned char) i;
+            j++;
     }
 
     shuffle(adminCode);
     printcode(adminCode);
-    resetCodeInitial(adminCode);
+    shuffle(resetCode);
+    printcode(resetCode);
 }
