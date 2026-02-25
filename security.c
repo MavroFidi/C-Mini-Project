db27eead9a9d58e404aa58f9eaf9355d450fa922
MavroFidi <thepixypoxy@gmail.com>
2026-02-25 20:43:30 +0530

    Refactor security.c for improved readability

--- security.c
+++ security.c
@@ -4,25 +4,30 @@
 #include "security.h"
 #include "admin.h"
 
-
-unsigned char (*initializeCodes(int numArrays))[CODE_LENGTH] {
+unsigned char (*initializeCodes(int numArrays)) [CODE_LENGTH]
+{
     unsigned char (*userCodes)[CODE_LENGTH] = malloc(numArrays * sizeof(*userCodes));
-    if (userCodes == NULL) return NULL;
-    
+    if (userCodes == NULL)
+        return NULL;
+
     srand(time(NULL));
-    for (int i = 0; i < numArrays; i++) {
-         for (int j = 0; j < CODE_LENGTH; j++) {
+    for (int i = 0; i < numArrays; i++)
+    {
+        for (int j = 0; j < CODE_LENGTH; j++)
+        {
             userCodes[i][j] = j;
         }
         shuffle(userCodes[i]);
     }
-    
+
     return userCodes;
 }
 
-void shuffle(unsigned char arr[CODE_LENGTH]) {
+void shuffle(unsigned char arr[CODE_LENGTH])
+{
 
-    for (int i = CODE_LENGTH-1; i > 0; i--) {
+    for (int i = CODE_LENGTH - 1; i > 0; i--)
+    {
         int j = rand() % (i + 1);
 
         unsigned char temp = arr[i];
@@ -31,26 +36,35 @@ void shuffle(unsigned char arr[CODE_LENGTH]) {
     }
 }
 
-void printcode(int a) {
-    for (int i = 0; i < a; i++) {
+void printcode(int a)
+{
+    for (int i = 0; i < a; i++)
+    {
         printf("User Code %i: ");
-        for (int j = 0; ; j < 222; j++) {
+        for (int j = 0;; j < 222; j++)
+        {
             printf("%c", usercode[i][j]);
         }
         printf("\n");
     }
 }
 
+void main()
+{
+    char *admincode;
+    printf("Enter the admin code: ");
+    scanf("%s", admincode);
 
-void main(int argc, char *argv[]) {
-    if (memcmp(argv[1], adminCode, 222) == 0) {
+    if (memcmp(argv[1], adminCode, 222) == 0)
+    {
         int numArrays;
-        printf("Enter the number of user codes you wish to initialize. ")
+        printf("Enter the number of user codes you wish to initialize. ");
         scanf("%i", &numArrays);
         unsigned char (*userCodes)[CODE_LENGTH] = initializeCodes(numArrays);
-        printcode(numArrays)
+        printcode(numArrays);
     }
-    else {
+    else
+    {
         printf("Access Denied\n");
     }
 }
