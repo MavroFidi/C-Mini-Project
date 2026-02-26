bb4da7047b5ead4e0c164b4a554e9ca4b1b1f9fc
MavroFidi <thepixypoxy@gmail.com>
2026-02-26 14:40:50 +0530

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,7 +1,7 @@
 #ifndef ADMIN_H
 #define ADMIN_H
 
-#define ADMIN_CODE_LENGTH 222
+#define ADMIN_CODE_LENGTH 1
 
 extern unsigned char adminCode[ADMIN_CODE_LENGTH];
 extern unsigned char resetCode[ADMIN_CODE_LENGTH];
@@ -10,5 +10,6 @@ void generate_admin_codes(void);
 void save_admin_codes(void);
 int load_admin_codes(void);
 void shuffle(unsigned char arr[ADMIN_CODE_LENGTH]);
+void printcode(unsigned char arr[ADMIN_CODE_LENGTH]);
 
 #endif
