5780daa18e5eaf776e38f0943bf730b755ad6003
MavroFidi <thepixypoxy@gmail.com>
2026-02-26 14:40:34 +0530

    Update admin.c

--- admin.c
+++ admin.c
@@ -3,70 +3,57 @@
 #include <time.h>
 #include "admin.h"
 
-unsigned char adminCode[222];
-unsigned char resetCode[222];
+unsigned char adminCode[1];
+unsigned char resetCode[1];
 
-
-void shuffle(unsigned char adminCode[222]) {
-
-    for (int i = 221; i > 0; i--) {
+void shuffle(unsigned char arr[ADMIN_CODE_LENGTH]) {
+    for (int i = ADMIN_CODE_LENGTH - 1; i > 0; i--) {
         int j = rand() % (i + 1);
-
-        unsigned char temp = adminCode[i];
-        adminCode[i] = adminCode[j];
-        adminCode[j] = temp;
+        unsigned char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
 
-void printcode(unsigned char adminCode[222]) {
-    for (int i = 0; i < 222; i++) {
-        
-        printf("%c",(char)adminCode[i]);
+void printcode(unsigned char arr[ADMIN_CODE_LENGTH]) {
+    for (int i = 0; i < ADMIN_CODE_LENGTH; i++) {
+        printf("%c", (char)arr[i]);
     }
 }
 
-void save_admin_codes()
-{
+void save_admin_codes(void) {
     FILE *f = fopen("admin.bin", "wb");
     if (!f) {
         printf("Failed to save admin codes\n");
         exit(1);
     }
-
-    fwrite(adminCode, sizeof(unsigned char), 222, f);
-    fwrite(resetCode, sizeof(unsigned char), 222, f);
-
+    fwrite(adminCode, sizeof(unsigned char), ADMIN_CODE_LENGTH, f);
+    fwrite(resetCode, sizeof(unsigned char), ADMIN_CODE_LENGTH, f);
     fclose(f);
 }
 
-int load_admin_codes() {
+int load_admin_codes(void) {
     FILE *f = fopen("admin.bin", "rb");
     if (!f) {
         printf("Admin file missing!\n");
         return 0;
     }
-
-    fread(adminCode, sizeof(unsigned char), 222, f);
-    fread(resetCode, sizeof(unsigned char), 222, f);
-
+    fread(adminCode, sizeof(unsigned char), ADMIN_CODE_LENGTH, f);
+    fread(resetCode, sizeof(unsigned char), ADMIN_CODE_LENGTH, f);
     fclose(f);
     return 1;
 }
 
-void generate_admin_codes() {
-    srand(time(NULL));
+void generate_admin_codes(void) {
+    srand((unsigned int)time(NULL));
 
     int j = 0;
     for (int i = 33; i < 256; i++) {
-            adminCode[j] = (unsigned char) i;
-            j++;
-    }
-    j=0;
-    for (int i = 33; i < 256; i++) {
-            resetCode[j] = (unsigned char) i;
-            j++;
+        adminCode[j] = (unsigned char)i;
+        resetCode[j] = (unsigned char)i;
+        j++;
     }
 
     shuffle(adminCode);
-    shuffle(resetCode); 
+    shuffle(resetCode);
 }
