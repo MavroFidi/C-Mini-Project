cc82bb7f7d4ca56c9c7d1b22a1c1b94f49b9b991
MavroFidi <thepixypoxy@gmail.com>
2026-02-26 14:42:25 +0530

    Update security.h

--- security.h
+++ security.h
@@ -3,7 +3,12 @@
 
 #define CODE_LENGTH 50
 
+void initialize_security(void);
+int  verify_admin(unsigned char *input);
+void admin_panel(void);
+
 unsigned char (*initializeCodes(int numArrays))[CODE_LENGTH];
+void printUserCodes(int count, unsigned char (*userCodes)[CODE_LENGTH]);
 void cleanupCodes(void *ptr);
 
 #endif
