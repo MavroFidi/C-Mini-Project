b6c28a81f70110c47f62c533bfe5e01fffe8b6e5
MavroFidi <thepixypoxy@gmail.com>
2026-02-26 13:31:37 +0530

    Use ADMIN_CODE_LENGTH for adminCode and resetCode
    Refactor adminCode and resetCode declarations to use a defined length constant.

--- admin.h
+++ admin.h
@@ -1,8 +1,14 @@
 #ifndef ADMIN_H
 #define ADMIN_H
 
-extern unsigned char adminCode[222];
-extern unsigned char resetCode[222];
+#define ADMIN_CODE_LENGTH 222
 
+extern unsigned char adminCode[ADMIN_CODE_LENGTH];
+extern unsigned char resetCode[ADMIN_CODE_LENGTH];
+
+void generate_admin_codes(void);
+void save_admin_codes(void);
+int load_admin_codes(void);
+void shuffle(unsigned char arr[ADMIN_CODE_LENGTH]);
 
 #endif
