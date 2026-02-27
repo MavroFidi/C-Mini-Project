cb2dcd86280c41639ff38eba40add5bec6879d64
MavroFidi <thepixypoxy@gmail.com>
2026-02-27 13:59:13 +0530

    Update database.h

--- database.h
+++ database.h
@@ -1,10 +1,13 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
-void open_db(void);
-void close_db(void);
+#include "users.h"
+
 int  init_database(void);
 void close_database(void);
-void user_login(void);
+void save_user(User *user);
+int  find_user(const char *name, const char *passcode, User *out_user);
+void notes_menu(const char *username);
+void reset_users(void);
 
 #endif
