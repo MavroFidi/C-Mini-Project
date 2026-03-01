63addbd1883906c65cf5922e043acd07eee47fff
MavroFidi <thepixypoxy@gmail.com>
2026-03-01 18:38:58 +0530

    Update users.h

--- users.h
+++ users.h
@@ -3,9 +3,10 @@
 
 typedef struct {
     char name[50];
-    char passcode[16];
+    char passcode[62];
 } User;
 
+void get_passcode_input(char *out_passcode);
 User *create_users(int n);
 void print_users(int n, User *users);
 void initialize_users(void);
