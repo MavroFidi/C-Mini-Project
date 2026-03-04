dc6b082a4557b4d3ff4df5b6a0106411968f2ba1
MavroFidi <thepixypoxy@gmail.com>
2026-03-04 19:48:54 +0530

    Update setup.c

--- setup.c
+++ setup.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include "admin.h"
 
-int main(void) {
+void main(void) {
     printf("Generating admin credentials...\n");
 
     generate_admin_codes();
     save_admin_codes();
 
-    printf("Admin code:  "); printcode(adminCode); printf("\n");
+    printf("Admin code:  ");
+
+    printf("\n");
+    
+    printcode(adminCode);
+    
+    printf("\n");
+    
     printf("\nAdmin credentials saved to admin.bin\n");
     printf("Keep admin.bin in the same folder as app.exe\n");
-
-    return 0;
 }
